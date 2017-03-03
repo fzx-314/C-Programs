f08ce944bae0864a6b2950b0dca82af98fb74d13
fzx-314 <maldekeval@ymail.com>
2017-03-03 13:22:58 +0530

    Add files via upload

---
+++ C programming Language by B.K - D.R/Chapter 2 Types- Operator- and Expressions/ELEMINAT.C
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+	char c;
+	char s1[10],s2[10];
+	int i=0,j,k,r,flag=0;
+	clrscr();
+	while((s1[i++]=getchar())!='\n');
+	i--;
+	j=i;
+	i=0;
+	while((s2[i++]=getchar())!='\n');
+	i--;
+	k=i;
+	i=0;
+	while(i!=j)
+	{
+	     flag=0;
+	     for(r=0;r!=k;r++)
+	     {
+		if(s1[i]==s2[r])
+		{
+			flag=1;
+			break;
+		}
+	     }
+	     if(flag==0)
+	     printf("%c",s1[i]);
+	     i++;
+	}
+       getch();
+}
