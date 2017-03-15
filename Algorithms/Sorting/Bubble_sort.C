20fe8acba39347fe40cc70c7201f88e005bc1df1
Keval Malde <maldekeval@ymail.com>
2017-03-15 19:59:17 +0530

    Create Bubble_sort.C

---
+++ Algorithms/Sorting/Bubble_sort.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i=0,j,temp=0,a[10];
+    for(;i<10;i++)
+    {
+        printf("Enter %d element\n",i+1);
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        temp=0;             //To check if swapping happend 
+        for(j=1;j<10;j++)
+        {
+            if(a[j]<a[j-1])
+            {
+                temp=a[j];
+                a[j]=a[j-1];
+                a[j-1]=temp;
+                temp=1;
+            }
+        }
+        if(temp==0)// in case array is already sorted then exit
+        {
+            break;
+        }
+    }
+    for(i=0;i<10;printf("%d\t",a[i++]));
+    return 0;
+}
