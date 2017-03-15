af8dcddd3f3d1af14dff61dd351092e1ba7cadfd
Keval Malde <maldekeval@ymail.com>
2017-03-15 19:23:30 +0530

    Update Merge_Sort.C

--- Algorithms/Sorting/Merge_Sort.C
+++ Algorithms/Sorting/Merge_Sort.C
@@ -1,32 +1,54 @@
 #include <stdio.h>
 void merge(int a[],int p,int q,int r)
 {
-    int n1,n2,L[10],R[10],i,j,k;
-    n1=q-p+1;
-    n2=r-q;
-    for(i=0;i<n1;i++)
-        L[i]=a[p+i];
-    for(i=0;i<n2;i++)
-        R[i]=a[q+j];
-    i=j=1;
-    for(k=p;k<r;k++)
-        if(L[i]<=R[j])
-            A[k]=L[i++];
-        else 
-            A[k]=R[j++];
+	int n1=q-p+1,n2=r-q,i,j,k;			//Size of left and right sub part Check logic q-p+1
+	int L[n1],R[n2];				//auxilary array
+	for(i=0;i<n1;i++)
+		L[i]=a[p+i];				//copying left subtree
+	for(i=0;i<n2;i++)
+		R[i]=a[q+1+i];				//copying righ subtree
+	i=j=0;
+	k=p;                            // k=p casue of bug here dont replace it
+	while(i<n1&&j<n2)				// to check if one of partition is empty or not
+	{
+		if(L[i]<=R[j])
+			a[k++]=L[i++];			//copying if left subtree has smallest element
+		else
+			a[k++]=R[j++];			//copying if right subtrr has smallest element
+	}
+	while(i<n1)						//if one if partiton is empty 
+		a[k++]=L[i++];					
+	while(j<n2)
+		a[k++]=R[j++];
 }
-void merge_Sort(int a[],int p,int r)
+void merge_sort(int a[],int p,int r)
 {
-    if(p<r)
-    {
-        q=[(p+r)/2];
-        merge_Sort(a,p,q);
-        merge_Sort(a,q+1,r);
-        merge(a,p,q,r);
-    }
+	int q;
+	if(p<r)				// Mainating Pointers
+	{
+		q=(p+r)/2;         // Logic might be wrong in some case
+		merge_sort(a,p,q);	//Creating left subtree
+		merge_sort(a,q+1,r);//Creating Right Subtree
+		merge(a,p,q,r);		// Conquering or merging Left & Right Sub tree
+	}
 }
-int main(void) {
-	
-		return 0;
+void print(int a[]) //Just another printing function
+{
+	int i=0;
+	for(;i<10;printf("%d\t",a[i++]));
 }
-
+int main()
+{
+	int a[10],i=0;			            //Intializing array
+	for(;i<10;i++)						// Getting Input
+	{
+		printf("Enter %d element\n",i+1);
+		scanf("%d",&a[i]);
+	}
+	printf("Given array is\n");
+	print(a);
+	merge_sort(a,0,10);// Recursive call to merge_sort function
+	printf("\nSorted array is\n");
+	print(a);
+	return 0;
+}
