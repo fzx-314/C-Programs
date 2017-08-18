d7312b99cd58905e4adce91f9039fe1725de84b9
Keval Malde <maldekeval@ymail.com>
2017-08-18 17:22:26 +0530

    Update min_max_in_unsorted.C

--- Algorithms/Application/min_max_in_unsorted.C
+++ Algorithms/Application/min_max_in_unsorted.C
@@ -1,65 +1,85 @@
 /*
   To find min and max in an unsorted array in O(n) time
 */
-#include <stdio.h>
-void Min_Max(int a[],int n)
-{
-    int i,j,min,max;
-    if(n%2==0)  // If input size is even then complexity is 3(n/2)-2
-    {
-        if(a[0]<=a[1])   // Order of O(1) operation
-        {
-            min=a[0];
-            max=a[1];
-        }
-        else
-        {
-            min=a[1];
-            max=a[0];
-        }
-        i=2;
-        j=3;
-    }
-    else        // If input size is odd then complexity is 3(n/2)
-    {
-        min=a[0];
-        max=a[0];
-        i=1;
-        j=2;
-    }
-    for(;i<n,j<n;i+2,j+2)
-    {
-        if(a[i]<=a[j])   //1st comparision
-        {
-            if(a[i]<=min) //2nd comparision
-            {
-                min=a[i];
-            }
-            if(a[j]>max)    // 3rd comparision
-            {
-                max=a[j];
-            }
-        }
-        else
-        {
-            if(a[j]<=min) //2nd comparision
-            {
-                min=a[j];
-            }
-            if(a[i]>max)// 3rd comparisiom
-            {
-                max=a[i];
-            }
-        }
-    }
-    printf("\nMin and Max are %d %d",min,max);
-}
-int main(void) {
-	int a[1000],i=0,n;
-	printf("Enter input size");
-	scanf("%d",&n);             // Getting Input size
-	for(;i<n;scanf("%d",&a[i++]));//Taking input
-	Min_Max(a,n);
+#include<stdio.h>
+#define n 11	// Change n for even or odd elements
+int a[n];
+int input(){	// accepting inputs
+	int i;
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
 	return 0;
 }
+int min_max(){
+	int small,large,count=0,i;	//variable decleration
+	if(n%2==0){					// for even value of n 
+		count++;
+		if(a[0]<a[1]){			
+		/* comparing first 2 elements of array least of them 
+		will be intialize to small and greatest will be intialize to large */
+			small=a[0];			
+			large=a[1];
+		}
+		else{
+			small=a[1];
+			large=a[0];
+		}
+		for(i=2;i<n;i=i+2){	// this loop will compare 2 elements of array and further compare with small and large to find min and max
+		    count++;
+			if(a[i]<a[i+1]){	
+			    count++;
+				if(a[i]<small){
+					small=a[i];
+				}
+				count++;
+				if(a[i+1]>large){
+					large=a[i+1];
+				}
+			}
+			else{
+				count++;
+				if(a[i+1]<small){
+					small=a[i+1];
+				}
+				count++;
+				if(a[i]>large){
+					large=a[i+1];
+				}
+			}
+		}
+	}
+	else{
+		small=large=a[0];
+		for(i=1;i<n;i=i+2){
+			count++;
+			if(a[i]<a[i+1]){
+			    count++;
+				if(a[i]<small){
+					small=a[i];
+				}
+				count++;
+				if(a[i+1]>large){
+					large=a[i+1];
+				}
+			}
+			else{
+				count++;
+				if(a[i+1]<small){
+					small=a[i+1];
+				}
+				count++;
+				if(a[i]>large){
+					large=a[i+1];
+				}
+			}
+		}
+	}
+	printf("Largest=%d\tSmallest=%d\tCount=%d",large,small,count);
+	return 0;
+}
+int main(){
 
+	input();
+	min_max();
+	return 0;
+}
