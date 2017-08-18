e627241052eb1a27672a87a3d11ba18d762667f2
Keval Malde <maldekeval@ymail.com>
2017-08-18 15:25:09 +0530

    Update Intersion_sort.C

--- Algorithms/Sorting/Intersion_sort.C
+++ Algorithms/Sorting/Intersion_sort.C
@@ -1,29 +1,19 @@
-#include <stdio.h>
-#include <math.h>
-void insertionSort(int arr[], int n)
-{
-   int i, key, j;
-   for (i = 1; i < n; i++)
-   {
-       key = arr[i];
-       j = i-1;
-       while (j >= 0 && arr[j] > key)
-       {
-           arr[j+1] = arr[j];
-           j = j-1;
-       }
-       arr[j+1] = key;
-   }
-}
-void main()
-{
-    int arr[] = {9,1,15,13,11};
-    int i;
-    int n = sizeof(arr)/sizeof(arr[0]);
-    clrscr();
-    insertionSort(arr, n);
-    for (i=0; i < n; i++)
-       printf("%d ", arr[i]);
-   printf("\n");
-   getch();
+#include<stdio.h>
+#define n 10
+int main(){
+	int a[n],key,i,j,temp;  // variable decleration
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);  // taking unsored input
+	for(j=1;j<n;j++){       // insertion sort begins
+	key=a[j];               //intialization of key element
+	i=j-1;
+	while(i>=0&&a[i]>key){  // finding element less then key
+	a[i+1]=a[i];            // making way for minimum element 
+	i--;
+	}
+	a[i+1]=key;             // giving elemnet its right place, bend the knee or drcarys 
+	}
+	for(i=0;i<n;i++)        // just another printing function
+	    printf("%d\t",a[i]);
+	return 0;
 }
