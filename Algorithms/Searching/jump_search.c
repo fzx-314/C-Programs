20b9b7bd02cef2efe833c79331c6fec20cedf625
Keval Malde <maldekeval@ymail.com>
2018-06-12 19:03:09 +0530

    Create jump_search.c

---
+++ Algorithms/Searching/jump_search.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+#define min(a,b) ((a<b)?(a):(b))
+#define max 10
+int jump_search(int a[],int m,int x){
+	int step=sqrt(m);
+	int prev=0;
+	if(a[min(step,m)-1]<x){
+		prev=step;
+		step+=sqrt(m);
+		if(prev>m)
+			return -1;
+	}
+	while(a[prev]<x){
+		prev++;
+		if(prev==min(step,m))
+			return -1;
+	}
+	if(a[prev]==x)
+		return prev;
+}
+int main(void){
+	int a[10],i,ans,x;
+	for(i=0;i<max;scanf("%d",&a[i++]));
+	scanf("%d",&x);
+	ans=jump_search(a,max,x);
+	printf("\n%d is at index %d\n",x,ans);
+}
